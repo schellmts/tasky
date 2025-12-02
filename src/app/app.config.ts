3ec0a81f3120570a67bd635bd610eef1aa0bcdb8
Matheus Santos <matheus.emiliano@hotmail.com>
2025-12-02 20:07:09 -0300

    downdgrade angular

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,12 +6,15 @@ import {
   provideHttpClient,
   withInterceptorsFromDi,
 } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
+import { routes } from './app.routes';
+
 export const appConfig: ApplicationConfig = {
   providers: [
-    { provide: APP_BASE_HREF, useValue: '/portal/2/NOMESISTEMA' },
-    provideHttpClient(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    { provide: APP_BASE_HREF, useValue: '/portal/2/taskyc' },
     provideHttpClient(withInterceptorsFromDi()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
   ],
 };
