55ee233690485f9b72a282fec6b6a8a008246e5b
Matheus Santos <matheus.emiliano@hotmail.com>
2025-12-02 10:13:25 -0300

    add inv vision

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { TicketList } from './ticket-list/ticket-list';
 import { KanbanBoard } from './kanban-board/kanban-board';
 import { CreateTicket } from './create-ticket/create-ticket';
 import { TicketDetail } from './ticket-detail/ticket-detail';
+import { Inventory } from './inventory/inventory';
 
 export const routes: Routes = [
   { path: '', component: ServiceDeskDashboard },
@@ -11,5 +12,6 @@ export const routes: Routes = [
   { path: 'tickets', component: TicketList },
   { path: 'tickets/create', component: CreateTicket },
   { path: 'tickets/:id', component: TicketDetail },
-  { path: 'kanban', component: KanbanBoard }
+  { path: 'kanban', component: KanbanBoard },
+  { path: 'inventory', component: Inventory }
 ];
---
+++ src/app/layout/layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [CommonModule, RouterModule],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css',
+})
+export class Layout {
+}
